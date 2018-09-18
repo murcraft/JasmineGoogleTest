f72ce0dbc14bdf0937123ae725023fc4cc052b2d
murcraft <ogulikpurse@gmail.com>
2018-09-18 16:11:00 +0300

    Were added locators to pages

--- spec/page_object_spec.js
+++ spec/page_object_spec.js
@@ -8,6 +8,10 @@ describe("Test searching on Google", function() {
         jasmine.DEFAULT_TIMEOUT_INTERVAL = 15000;
     });
 
+    afterAll(function(){
+        resultsPage.closePage();
+    })
+
     it("Check main page", async function () {
         await searchPage.openPage();
         let title = await searchPage.getPageTitle();
@@ -17,13 +21,14 @@ describe("Test searching on Google", function() {
 
     it("Go to results page", async function () {
         resultsPage = await searchPage.textRequireInField();
+        console.log(toString.call(resultsPage));
         let title = await resultsPage.getPageTitle();
         console.log(title);
         expect(title.toLowerCase()).toMatch("itechart");
     });
 
     it("Searching ITechArt titles in resuls", async function () {
-        let titles = await this.resultsPage.getResulsHeaders();
+        let titles = await resultsPage.getResulsHeaders();
         console.log(titles);
 
         // console.log("Number of titles: " + size);
