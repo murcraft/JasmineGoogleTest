f63c2fa1d59395124abe9d71592f1642ad2390f8
murcraft <ogulikpurse@gmail.com>
2018-09-19 16:44:40 +0300

    Were edited locators

--- spec/page_object_spec.js
+++ spec/page_object_spec.js
@@ -6,7 +6,7 @@ let searchPage;
 let resultsPage;
 let driver = null;
 const expectTitleSearchingPage = "google";
-const expectedTitleResultsPage = "itechart";
+const requestedWord = "itechart";
 
 describe("Test searching on Google", function () {
 
@@ -23,28 +23,28 @@ describe("Test searching on Google", function () {
   it("Check main page", async function () {
     await searchPage.openPage();
     let titleOfSearchingPage = await searchPage.getPageTitle();
-    await expect(titleOfSearchingPage.toLowerCase()).toBe(expectTitleSearchingPage);
+    await expect(titleOfSearchingPage.toLowerCase()).toEqual(expectTitleSearchingPage);
   });
 
   it("Check results page", async function () {
     await searchPage.textRequireInField();
     resultsPage = await new ResultsPage(driver);
     let titleOfResultsPage = await resultsPage.getPageTitle();
-    await expect(titleOfResultsPage.toLowerCase()).toMatch(expectedTitleResultsPage);
+    await expect(titleOfResultsPage.toLowerCase()).toMatch(requestedWord, "Title of results page contain");
   });
 
   it("Searching ITechArt titles in results", async function () {
     let titles = await resultsPage.getResulsHeaders();
     await resultsPage.printResultsHeaders();
     titles.forEach(async value => {
-      await expect(value.toLowerCase()).toMatch("itechart");
+      await expect(value.toLowerCase()).toMatch(requestedWord, "The requested word");
     });
 
   });
 
   it("Searching common amount of results", async function () {
     let results = await resultsPage.getNumberOfResults();
-    await expect(results).toBeGreaterThan(10000);
+    await expect(results).toBeGreaterThan(10000, "Number of results");
   });
 
 
