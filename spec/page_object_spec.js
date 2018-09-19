6e0dbfe3a7cea65467138386fc5a3462fabc7c91
murcraft <ogulikpurse@gmail.com>
2018-09-19 14:49:19 +0300

    Was deleted use_strict

--- spec/page_object_spec.js
+++ spec/page_object_spec.js
@@ -1,4 +1,3 @@
-'use strict';
 let SearchPage = require("../lib/page/SearchPage");
 let resultsPage = require("../lib/page/ResultsPage");
 let DriverHandler = require("../lib/driver/DriverHandler");
@@ -7,47 +6,47 @@ let driver = null;
 const titlePage = "google";
 const titleOfResultsPage = "itechart";
 
-describe("Test searching on Google", function() {
+describe("Test searching on Google", function () {
 
-    beforeAll(async function() {
-        driver = await DriverHandler.getInstance();
-        this.searchPage = await new SearchPage(driver);
-        await this.searchPage.openPage();
+  beforeAll(async function () {
+    driver = await DriverHandler.getInstance();
+    this.searchPage = await new SearchPage(driver);
+    await this.searchPage.openPage();
+  });
+
+  afterAll(async function () {
+    await DriverHandler.closeDriver();
+  });
+
+  beforeEach(function () {
+    jasmine.DEFAULT_TIMEOUT_INTERVAL = 15000;
+  });
+
+  it("Check main page", async function () {
+    let checkTrue = await this.searchPage.isSuitablePageTitle(titlePage);
+    expect(checkTrue).toBeTruthy();
+  });
+
+  it("Check results page", async function () {
+    resultsPage = await this.searchPage.textRequireInField();
+    let checkTrue = await resultsPage.isSuitablePageTitle(titleOfResultsPage);
+    expect(checkTrue).toBeTruthy();
+  });
+
+  it("Searching ITechArt titles in results", async function () {
+    let titles = await resultsPage.getResulsHeaders();
+    await resultsPage.printResultsHeaders();
+    titles.forEach(value => {
+      expect(value.toLowerCase()).toMatch("itechart");
     });
 
-    afterAll(async function(){
-        await DriverHandler.closeDriver();
-    });
+  });
 
-    beforeEach(function() {
-        jasmine.DEFAULT_TIMEOUT_INTERVAL = 15000;
-    });
-
-    it("Check main page", async function () {
-        let checkTrue = await this.searchPage.isSuitablePageTitle(titlePage);
-        expect(checkTrue).toBeTruthy();
-    });
-
-    it("Check results page", async function () {
-        resultsPage = await this.searchPage.textRequireInField();
-        let checkTrue = await resultsPage.isSuitablePageTitle(titleOfResultsPage);
-        expect(checkTrue).toBeTruthy();
-    });
-
-    it("Searching ITechArt titles in results", async function () {
-        let titles = await resultsPage.getResulsHeaders();
-        await resultsPage.printResultsHeaders();
-        titles.forEach(value => {
-            expect(value.toLowerCase()).toMatch("itechart");
-        });
-
-    });
-
-    it("Searching common amount of results", async function () {
-        let results = await resultsPage.getNumberOfResults();
-        await resultsPage.printNumberOfResults();
-        expect(results).toBeGreaterThan(10000);
-    });
+  it("Searching common amount of results", async function () {
+    let results = await resultsPage.getNumberOfResults();
+    await resultsPage.printNumberOfResults();
+    expect(results).toBeGreaterThan(10000);
+  });
 
 
 });
