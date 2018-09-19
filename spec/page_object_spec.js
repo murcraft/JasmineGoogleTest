8f17aeb1df560210f86694bcaf15f36463a17a46
murcraft <ogulikpurse@gmail.com>
2018-09-19 15:58:07 +0300

    Were edited test to check expected titles

--- spec/page_object_spec.js
+++ spec/page_object_spec.js
@@ -2,32 +2,33 @@ let SearchPage = require("../lib/page/SearchPage");
 let resultsPage = require("../lib/page/ResultsPage");
 let DriverHandler = require("../lib/driver/DriverHandler");
 
+let searchPage;
 let driver = null;
-const titlePage = "google";
-const titleOfResultsPage = "itechart";
+const expectTitleSearchingPage = "google";
+const expectTitleResultsPage = "itechart";
 
 describe("Test searching on Google", function () {
 
   beforeAll(async function () {
     jasmine.DEFAULT_TIMEOUT_INTERVAL = 15000;
-    driver = await DriverHandler.getInstance();
-    this.searchPage = new SearchPage(driver);
+    driver = await DriverHandler.GetInstance();
+    searchPage = new SearchPage(driver);
   });
 
   afterAll(async function () {
-    await DriverHandler.closeDriver();
+    await DriverHandler.CloseDriver();
   });
 
   it("Check main page", async function () {
-    await this.searchPage.openPage();
-    let checkTrue = await this.searchPage.isSuitablePageTitle(titlePage);
-    await expect(checkTrue).toBeTruthy();
+    await searchPage.openPage();
+    let titleOfSearchingPage = await searchPage.getPageTitle();
+    await expect(titleOfSearchingPage.toLowerCase()).toBe(expectTitleSearchingPage);
   });
 
   it("Check results page", async function () {
-    resultsPage = await this.searchPage.textRequireInField();
-    let checkTrue = await resultsPage.isSuitablePageTitle(titleOfResultsPage);
-    await expect(checkTrue).toBeTruthy();
+    resultsPage = await searchPage.textRequireInField();
+    let titleOfResultsPage = await resultsPage.getPageTitle();
+    await expect(titleOfResultsPage.toLowerCase()).toMatch(expectTitleResultsPage);
   });
 
   it("Searching ITechArt titles in results", async function () {
