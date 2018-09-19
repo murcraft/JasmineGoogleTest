cf968be2816228f4947a95cc191d32085915053b
murcraft <ogulikpurse@gmail.com>
2018-09-19 15:30:06 +0300

    Was edited code style

--- spec/page_object_spec.js
+++ spec/page_object_spec.js
@@ -9,35 +9,32 @@ const titleOfResultsPage = "itechart";
 describe("Test searching on Google", function () {
 
   beforeAll(async function () {
+    jasmine.DEFAULT_TIMEOUT_INTERVAL = 15000;
     driver = await DriverHandler.getInstance();
-    this.searchPage = await new SearchPage(driver);
-    await this.searchPage.openPage();
+    this.searchPage = new SearchPage(driver);
   });
 
   afterAll(async function () {
     await DriverHandler.closeDriver();
   });
 
-  beforeEach(function () {
-    jasmine.DEFAULT_TIMEOUT_INTERVAL = 15000;
-  });
-
   it("Check main page", async function () {
+    await this.searchPage.openPage();
     let checkTrue = await this.searchPage.isSuitablePageTitle(titlePage);
-    expect(checkTrue).toBeTruthy();
+    await expect(checkTrue).toBeTruthy();
   });
 
   it("Check results page", async function () {
     resultsPage = await this.searchPage.textRequireInField();
     let checkTrue = await resultsPage.isSuitablePageTitle(titleOfResultsPage);
-    expect(checkTrue).toBeTruthy();
+    await expect(checkTrue).toBeTruthy();
   });
 
   it("Searching ITechArt titles in results", async function () {
     let titles = await resultsPage.getResulsHeaders();
     await resultsPage.printResultsHeaders();
-    titles.forEach(value => {
-      expect(value.toLowerCase()).toMatch("itechart");
+    titles.forEach(async value => {
+      await expect(value.toLowerCase()).toMatch("itechart");
     });
 
   });
@@ -45,7 +42,7 @@ describe("Test searching on Google", function () {
   it("Searching common amount of results", async function () {
     let results = await resultsPage.getNumberOfResults();
     await resultsPage.printNumberOfResults();
-    expect(results).toBeGreaterThan(10000);
+    await expect(results).toBeGreaterThan(10000);
   });
 
 
