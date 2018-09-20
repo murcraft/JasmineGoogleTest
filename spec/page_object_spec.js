d618f01350a19dacd4461668523dd01248cd8c45
murcraft <ogulikpurse@gmail.com>
2018-09-20 23:39:12 +0300

    was added reader for JSON and dependency wc

--- spec/page_object_spec.js
+++ spec/page_object_spec.js
@@ -2,21 +2,31 @@
 let SearchPage = require('../lib/page/SearchPage')
 let ResultsPage = require('../lib/page/ResultsPage')
 let DriverHandler = require('../lib/driver/DriverHandler')
-const fs = require('fs')
+const fs = require('mz/fs')
 
-const data = require('./testData.json')
-
+let dataTest
 let searchPage
 let resultsPage
 let driver
 const expectedTitleMainPage = 'google'
 const requestedWord = 'itechart'
+const pathTestData = './spec/testData.json'
+
 describe('Test searching on Google', function () {
+  async function myReadfile () {
+    try {
+      let test = await fs.readFile(pathTestData, 'utf8')
+      return JSON.parse(test)
+    }
+    catch (err) { console.error(err) }
+  }
 
   beforeAll(async function () {
     jasmine.DEFAULT_TIMEOUT_INTERVAL = 15000
     driver = DriverHandler.GetInstance()
     searchPage = new SearchPage(driver)
+    dataTest = await myReadfile()
+
   })
 
   afterAll(async function () {
@@ -24,37 +34,35 @@ describe('Test searching on Google', function () {
   })
 
   it('Open main page and verify title', async function () {
-
-    let varir = await fs.readFile('D:\\Project\\JasmineGoogleTest\\spec\\testData.json', 'utf8', function (err, data) {
-
-      console.log('AAAAA', data)
-      return data
-    });
-    console.log('BBBBB', varir)
-    // let obj = JSON.parse(data);
+    await console.log(dataTest['1']['request'])
     await searchPage.navigate()
     let titleOfSearchingPage = await searchPage.getPageTitle()
-    await expect(titleOfSearchingPage.toLowerCase()).toEqual(expectedTitleMainPage, 'Title of searching page')
+    await expect(titleOfSearchingPage.toLowerCase()).
+      toEqual(expectedTitleMainPage, 'Title of searching page')
   })
 
   it('Search for keyword and verify title of results page', async function () {
-    await searchPage.enterRequiredWord(requestedWord)
+    await searchPage.enterRequiredWord(dataTest['1']['request'])
     resultsPage = new ResultsPage(driver)
     let titleOfResultsPage = await resultsPage.getPageTitle()
-    await expect(titleOfResultsPage.toLowerCase()).toContain(requestedWord, 'Title of results page')
+    await expect(titleOfResultsPage.toLowerCase()).
+      toContain(requestedWord, 'Title of results page')
   })
 
-  it('Check the page headers for matching the requested word', async function () {
-    let titles = await resultsPage.getResulsHeaders()
-    titles.forEach(async value => {
-      await expect(value.toLowerCase()).toMatch(requestedWord, 'The requested word')
+  it('Check the page headers for matching the requested word',
+    async function () {
+      let titles = await resultsPage.getResulsHeaders()
+      titles.forEach(async value => {
+        await expect(value.toLowerCase()).
+          toMatch(requestedWord, 'The requested word')
+      })
+
     })
 
-  })
-
-  it('Search for total results and check that this number is more than min', async function () {
-    let totalResults = await resultsPage.getNumberOfResults()
-    await expect(totalResults).toBeGreaterThan(10000, 'Min number of results')
-  })
+  it('Search for total results and check that this number is more than min',
+    async function () {
+      let totalResults = await resultsPage.getNumberOfResults()
+      await expect(totalResults).toBeGreaterThan(10000, 'Min number of results')
+    })
 
 })
