870d345320ace73ef81075f037cda0e9bd1a22ac
murcraft <ogulikpurse@gmail.com>
2018-09-21 09:13:59 +0300

    was deleted console log in test

--- spec/page_object_spec.js
+++ spec/page_object_spec.js
@@ -26,7 +26,6 @@ describe('Test searching on Google', function () {
     driver = DriverHandler.GetInstance()
     searchPage = new SearchPage(driver)
     dataTest = await myReadfile()
-
   })
 
   afterAll(async function () {
@@ -34,29 +33,24 @@ describe('Test searching on Google', function () {
   })
 
   it('Open main page and verify title', async function () {
-    await console.log(dataTest['1']['request'])
     await searchPage.navigate()
     let titleOfSearchingPage = await searchPage.getPageTitle()
-    await expect(titleOfSearchingPage.toLowerCase()).
-      toEqual(expectedTitleMainPage, 'Title of searching page')
+    await expect(titleOfSearchingPage.toLowerCase()).toEqual(expectedTitleMainPage, 'Title of searching page')
   })
 
   it('Search for keyword and verify title of results page', async function () {
     await searchPage.enterRequiredWord(dataTest['1']['request'])
     resultsPage = new ResultsPage(driver)
     let titleOfResultsPage = await resultsPage.getPageTitle()
-    await expect(titleOfResultsPage.toLowerCase()).
-      toContain(requestedWord, 'Title of results page')
+    await expect(titleOfResultsPage.toLowerCase()).toContain(requestedWord, 'Title of results page')
   })
 
   it('Check the page headers for matching the requested word',
     async function () {
       let titles = await resultsPage.getResulsHeaders()
       titles.forEach(async value => {
-        await expect(value.toLowerCase()).
-          toMatch(requestedWord, 'The requested word')
+        await expect(value.toLowerCase()).toMatch(requestedWord, 'The requested word')
       })
-
     })
 
   it('Search for total results and check that this number is more than min',
