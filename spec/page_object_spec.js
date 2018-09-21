910c3cfe488273998c16ff3ab3415f3191780bf8
murcraft <ogulikpurse@gmail.com>
2018-09-21 12:49:12 +0300

    was added data provider to check requested words

--- spec/page_object_spec.js
+++ spec/page_object_spec.js
@@ -14,6 +14,7 @@ const requestedWord = 'itechart'
 const pathTestData = './spec/testData.json'
 
 describe('Test searching on Google', function () {
+
   function myReadfile () {
     try {
       let test = fs.readFileSync(pathTestData, 'utf8')
@@ -22,14 +23,12 @@ describe('Test searching on Google', function () {
     catch (err) { console.error(err) }
   }
 
-
-  using({request: "itechart", count: "10000"}, function (data){
+  using(dataTest = myReadfile(), function (data) {
 
     beforeAll(async function () {
       jasmine.DEFAULT_TIMEOUT_INTERVAL = 15000
-      driver = DriverHandler.GetInstance()
+      driver = await DriverHandler.GetInstance()
       searchPage = new SearchPage(driver)
-      dataTest = await myReadfile()
     })
 
     afterAll(async function () {
@@ -38,31 +37,30 @@ describe('Test searching on Google', function () {
     })
 
     it('Open main page and verify title', async function () {
-      await console.log(dataTest)
       await searchPage.navigate()
       let titleOfSearchingPage = await searchPage.getPageTitle()
       await expect(titleOfSearchingPage.toLowerCase()).toEqual(expectedTitleMainPage, 'Title of searching page')
     })
 
     it('Search for keyword and verify title of results page', async function () {
-      await searchPage.enterRequiredWord(dataTest['0']['request'])
+      await searchPage.enterRequiredWord(data.request)
       resultsPage = new ResultsPage(driver)
       let titleOfResultsPage = await resultsPage.getPageTitle()
-      await expect(titleOfResultsPage.toLowerCase()).toContain(requestedWord, 'Title of results page')
+      await expect(titleOfResultsPage.toLowerCase()).toContain(data.request, 'Title of results page')
     })
 
     it('Check the page headers for matching the requested word',
       async function () {
         let titles = await resultsPage.getResulsHeaders()
         titles.forEach(async value => {
-          await expect(value.toLowerCase()).toMatch(requestedWord, 'The requested word')
+          await expect(value.toLowerCase()).toMatch(data.request, 'The requested word')
         })
       })
 
     it('Search for total results and check that this number is more than min',
       async function () {
         let totalResults = await resultsPage.getNumberOfResults()
-        await expect(totalResults).toBeGreaterThan(10000, 'Min number of results')
+        await expect(totalResults).toBeGreaterThan(data.count, 'Min number of results')
       })
 
   })
